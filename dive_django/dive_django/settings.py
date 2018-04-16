504e3a4b8ebb22b8d82c2415bd01c09e83bfa98d
daxinte <daxinte@gmail.com>
2018-04-17 00:08:31 +0300

    Models and Playing with the API

--- dive_django/dive_django/settings.py
+++ dive_django/dive_django/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'ovn=gr60ggs$=1e1@&73^#a8tz72c#x=7z1kim1xp0us97!&7o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'dive_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'ubuntu',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
