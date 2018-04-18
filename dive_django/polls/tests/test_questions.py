e91ddee793026cd4be2b488f169bb14c18000c73
daxinte <daxinte@gmail.com>
2018-04-18 14:19:53 +0300

    Update comment

--- dive_django/polls/tests/test_questions.py
+++ dive_django/polls/tests/test_questions.py
@@ -30,8 +30,8 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_recent_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
+        was_published_recently() returns True for questions whose pub_date
+        is less than or equal to 1 day.
         """
         now = timezone.now()
         pub_date = now - datetime.timedelta(hours=23)
